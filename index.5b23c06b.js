95e033143887a444394a20ddf24b6b8e1b7fdb8c
Wojtek <wojciechsadocha@gmail.com>
2023-12-19 12:41:43 +0100

    make build

---
+++ index.5b23c06b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),l=.5*t.clientHeight-.5*e.clientHeight,i=.5*t.clientWidth-.5*e.clientWidth;e.style.left=i+"px",e.style.top=l+"px";
+//# sourceMappingURL=index.5b23c06b.js.map
